9ebbace15f5ca4004273067e02523aea64079383
f111fei <f111fei1@163.com>
2014-12-22 17:31:10 +0800

    更新至版本1.5.0。删除IconButton

--- src/components/IconButton.ts
+++ src/components/IconButton.ts
@@ -12,38 +12,5 @@ module game {
             super();
             this.skinName = skin.components.IconButtonSkin;
         }
-
-        /**
-         * 要显示的图片
-         */
-        public iconDisplay:egret.gui.UIAsset;
-
-        private _icon:any;
-        /**
-         * 图标
-         */
-        public get icon():any
-        {
-            return this._icon;
-        }
-
-        public set icon(value:any)
-        {
-            if(value == this._icon)
-                return;
-            this._icon = value;
-            if(this.iconDisplay){
-                this.iconDisplay.source = value;
-            }
-        }
-
-        public partAdded(partName:string, instance:any):void{
-            super.partAdded(partName, instance);
-
-            if (instance == this.iconDisplay){
-                this.iconDisplay.source = this._icon;
-            }
-        }
-
     }
 }
